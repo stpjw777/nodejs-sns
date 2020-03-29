40a962cd2a49c35f29d83b674854729687d1c3b5
jaewan <stpjw777@gmail.com>
2020-03-29 21:51:11 +0900

    pugsetting

--- routes/page.js
+++ routes/page.js
@@ -2,8 +2,25 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/profile", (req, res) => {
+  res.render("profile", { title: "내 정보 - NodeBird", user: null });
+});
+
+router.get("/join", (req, res) => {
+  res.render("join", {
+    title: "회원가입 - NodeBird",
+    user: null,
+    joinError: req.flash("joinError")
+  });
+});
+
 router.get("/", (req, res) => {
-  res.send("Hello Node");
+  res.render("main", {
+    title: "NodeBird",
+    twits: [],
+    user: null,
+    loginError: req.flash("loginError")
+  });
 });
 
 module.exports = router;
